f2db1f50e4d195067b5506cf74e83748b314c0aa
jessebenjamin <jesse@Jesse-Benjamins-MacBook-Pro.local>
2014-01-16 13:13:14 +0100

    leapoly v2

--- js/leapoly.js
+++ js/leapoly.js
@@ -11,15 +11,12 @@ $(document).ready(function (){
       //$('.vvvv_img').css("height", boxH);
       console.log("tight!");
     }
-
-    var run = true;
     
     setTimeout(function () { 
 
       var img;
       var maxImages = 1000;
 
-      if (run = true) {
       for (var i = 1; i < maxImages; i++) {
 
         var src = "./images/leapoly" + i + ".png";
@@ -27,8 +24,14 @@ $(document).ready(function (){
         img.src = src;
 
         $(img).wrap("<a></a>").parent().attr("href", src).attr("alt", "Leapolized you!").attr("target", "_blank").wrap("<div class='vvvv_img'></div>").parent().appendTo( "#container" );
-
       }
+
+      var image = document.getElementById('.vvvv_img img');
+
+      $(image).onerror = function () {
+        $(this).css("display", "none");
+      };
+
     }, 500);
 
     var tcount = $("#dlist li:not(:has(.hiddenItem))").length;
